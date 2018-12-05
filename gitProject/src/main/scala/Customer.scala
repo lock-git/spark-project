18d15b666446da141784cc735ce04a4146cbc042
lock-git <Lock13177005211@foxmail.com>
2018-12-05 15:53:52 +0800

    一览众山小

---
+++ gitProject/src/main/scala/Customer.scala
@@ -0,0 +1,14 @@
+/**
+  *
+  * author:Lock.xia
+  * date:2018/12/5 - 15:44
+  *
+  */
+class Customer {
+
+}
+object Wonderful{
+  def main(args: Array[String]): Unit = {
+    println("nzuibang!")
+  }
+}
